260b56aa96c47a305d881fc55ed61c162f552954
hanwckf <my375229675@gmail.com>
2018-04-10 22:18:06 +0800

    error handle

--- rootfs/build-debian-armhf.sh
+++ rootfs/build-debian-armhf.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 rootfs="debian-rootfs"
 [ -d "$rootfs" ] && echo "dir exists" && exit 0
+command -v debootstrap >/dev/null 2>&1 || { echo >&2 "debootstrap not found"; exit 1; }
+[ ! -f /usr/bin/qemu-arm-static ] && { echo >&2 "qemu-arm-static not found"; exit 1; }
 
 sudo debootstrap --components=main,contrib,non-free \
 --include=nano,openssh-server,busybox-syslogd,dialog,sysvinit-core,locales,htop,mtd-utils,iptables,ipset,ntp,usbutils \
